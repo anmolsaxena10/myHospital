59abba557abaee9f0b628fb8fd429e102348b150
Anmol Saxena <anmol.saxena10@gmail.com>
2018-04-06 05:04:48 +0530

    refactored Bill App

--- bill/views.py
+++ bill/views.py
@@ -8,6 +8,7 @@ from stock.models import items, stock
 from datetime import datetime
 from home.context_processors import hasGroup
 from case.models import case
+from django.contrib import messages
 # Create your views here.
 
 #CREATE
@@ -19,6 +20,7 @@ def generate(request, case_id):
         c['case'] = case.objects.get(id=int(case_id))
         c['items'] = items.objects.all()
         return render(request, 'bill/generate.html', c)
+    messages.add_message(request, messages.WARNING, 'Access Denied.')
     return HttpResponseRedirect('home/')
 
 @login_required
@@ -32,6 +34,9 @@ def doGenerate(request):
         ammount = item.sell_price * quantity
         b = bill(case=c, item=item, quantity=quantity, bill_date=bill_date, bill_details=bill_details, ammount=ammount)
         b.save()
+        messages.add_message(request, messages.INFO, 'Successfully Generated Bill.')
+        return HttpResponseRedirect('/bill/')
+    messages.add_message(request, messages.WARNING, 'Access Denied.')
     return HttpResponseRedirect('/home/')
 
 #RETRIEVE
@@ -41,6 +46,7 @@ def view(request):
     c.update(csrf(request))
     if hasGroup(request.user, 'patient'):
         c['bills'] = []
+        c['isPatient'] = True
         for cases in case.objects.filter(patient=request.user):
             c['bills'].extend(list(bill.objects.filter(case=cases)))
     elif hasGroup(request.user, 'receptionist'):
@@ -48,10 +54,20 @@ def view(request):
         if id == '':
             c['selectPatient'] = True
             c['patients'] = User.objects.filter(groups__name='patient')
+            return render(request, 'bill/view_bill.html', c)
         else:
             c['bills'] = []
             for cases in case.objects.filter(patient=User(id=id)):
                 c['bills'].extend(list(bill.objects.filter(case=cases)))
+    bills = c['bills']
+    c['paidBills'] = []
+    c['pendingBills'] = []
+    for b in bills:
+        if b.is_paid:
+            c['paidBills'].append(b)
+        else:
+            c['pendingBills'].append(b)
+    print(c['paidBills'])
     return render(request, 'bill/view_bill.html', c)
 
 @login_required
@@ -69,6 +85,9 @@ def pay(request):
             b = bill.objects.get(id=int(id))
             b.is_paid = True
             b.save()
+        messages.add_message(request, messages.INFO, 'Bill Paid Successfully.')
+        return HttpResponseRedirect('/bill/')
+    messages.add_message(request, messages.WARNING, 'Access Denied.')
     return HttpResponseRedirect('/home')
 
 #DELETE
@@ -77,4 +96,7 @@ def delete(request, id):
     user = request.user
     if hasGroup(user, 'receptionist'):
         bill.objects.get(id=id).delete()
+        messages.add_message(request, messages.INFO, 'Successfully Deleted Bill.')
+        return HttpResponseRedirect('/bill/')
+    messages.add_message(request, messages.WARNING, 'Access Denied.')
     return HttpResponseRedirect('/home')
