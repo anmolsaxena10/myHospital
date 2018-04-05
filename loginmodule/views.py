7b0a2e5e44c25fb5135b62fb58cf1054d32f012e
Anmol Saxena <anmol.saxena10@gmail.com>
2018-04-06 03:19:22 +0530

    refactored Login App

--- loginmodule/views.py
+++ loginmodule/views.py
@@ -3,10 +3,12 @@ from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.template.context_processors import csrf
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
 	if request.user.is_authenticated:
+		messages.add_message(request, messages.INFO, 'You are already Logged in.')
 		return HttpResponseRedirect('/home')
 	else:
 		c = {}
@@ -20,11 +22,15 @@ def auth_view(request):
 
 	if user is not None:
 		auth.login(request, user)
+		messages.add_message(request, messages.INFO, 'Your are now Logged in.')
 		return HttpResponseRedirect('/home')
 	else:
+		messages.add_message(request, messages.WARNING, 'Invalid Login Credentials.')
 		return HttpResponseRedirect('/login')
 
 def logout(request):
 	if request.user.is_authenticated:
 		auth.logout(request)
+	messages.add_message(request, messages.INFO, 'You are Successfully Logged Out')
+	messages.add_message(request, messages.INFO, 'Thanks for visiting.')
 	return HttpResponseRedirect('/login')
